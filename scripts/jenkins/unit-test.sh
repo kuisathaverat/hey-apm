88b0aa6127a25136ebb4c223b39109a16ec3ede5
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-01-04 15:17:37 +0100

    install go if it is not installed

--- scripts/jenkins/unit-test.sh
+++ scripts/jenkins/unit-test.sh
@@ -10,6 +10,7 @@ jenkins_setup
 export GOPATH=$WORKSPACE
 export PATH=$PATH:$GOPATH/bin
 eval "$(gvm ${GO_VERSION})"
+
 echo "Installing hey-apm dependencies and running unit tests..."
 go get -v -u github.com/golang/dep/cmd/dep
 dep ensure -v
